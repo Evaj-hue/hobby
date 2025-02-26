1d1c7751469a70b5dde1bb933773d2b65c620937
evaj-hue <christianjave.gustilo@outlook.com>
2025-02-26 23:25:11 +0800

    user redirect

--- dashboard.php
+++ dashboard.php
@@ -1,13 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: index.html');
+    exit();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+    <title>User Dashboard</title>
 </head>
 <body>
-    <h1>USERNAME MO GAGO</h1>
-    <a href="logout.php">Logout</a>
+    <?php include("partials/user_navbar.php"); ?> <!-- Sidebar included -->
+    <?php include("partials/user_sidebar.php"); ?> <!-- Sidebar included -->
 
 </body>
 </html>
