f68b7f2cce9df845bd8e829b79266f05f68fd0e6
evaj-hue <christianjave.gustilo@outlook.com>
2025-04-05 15:27:24 +0800

    rack dashboard implementation

---
+++ rack/config.php
@@ -0,0 +1,10 @@
+<?php $servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_management";
+// date_default_timezone_set("Asia/Kolkata");
+// $date=date("d/m/Y");
+// $time=date("h:i A");
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
